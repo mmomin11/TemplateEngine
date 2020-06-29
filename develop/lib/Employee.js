cb204cafde432928aac1b4ea97a029fbc62e1da8
mmomin11 <mubin.momin@nxp.com>
2020-06-29 13:57:22 -0500

    created classes for different employees

--- develop/lib/Employee.js
+++ develop/lib/Employee.js
@@ -1,1 +1,27 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email, role="Employee") {
+        this.name = name;
+        this.id = id; 
+        this.email = email;
+        this.role = role
+    }
+
+    getID() {
+        return this.id;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return this.role;
+    }
+}
+
+module.exports = Employee;
--- develop/lib/Engineer.js
+++ develop/lib/Engineer.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+
+class Engineer extends Employee{
+    constructor(id, name, email, username) {
+        super(id, name, email, username);
+        this.username = username;
+    }
+
+    getRole() {
+        return "Engineer";
+    }
+
+    getGithub() {
+        return this.username;
+    }
+}
+
+module.exports = Engineer;
